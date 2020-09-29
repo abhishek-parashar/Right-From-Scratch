b7f58ed2f51628673f1e32831cbdb38c0f0bdaca
abhishek-parashar <abhishekparashar@ieee.org>
2020-09-29 15:59:12 +0530

    backtracking

---
+++ Competitive Coding/Generate all subsets.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 29 15:51:01 2020
+
+@author: Abhishek Parashar
+"""
+
+def subsetutil(arr, subset, index):
+    print(*subset)
+    for i in range(len(arr)):
+        subset.append(arr[i])
+        subsetutil(arr,subset,index+1)
+        subset.pop(-1)
+    return
+def subset(arr):
+    global res
+    subset=[]
+    index=0
+    subsetutil(arr, subset, index)
+    return
+array=[1,2,3]
+subset(array)
