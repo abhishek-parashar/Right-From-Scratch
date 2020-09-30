28ca0d1d9b2da28386fb0de3fc773993d38658a5
abhishek-parashar <abhishekparashar@ieee.org>
2020-09-30 16:27:32 +0530

    Backtracking

--- Competitive Coding/unique-subsets-repeating-no.py
+++ Competitive Coding/unique-subsets-repeating-no.py
@@ -5,20 +5,19 @@ Created on Tue Sep 29 15:51:01 2020
 @author: Abhishek Parashar
 """
 
-def subsetutil(arr, subset, index):
-    print(*subset)
+def subsetutil(arr, subset, index,ans):
+    ans.append(subset[:])
     for i in range(index,len(arr)):
         if (i>index and arr[i]==arr[i-1]):
             continue
         subset.append(arr[i])
-        subsetutil(arr,subset,i+1)
+        subsetutil(arr,subset,i+1,ans)
         subset.pop(-1)
-    return
-def subset(arr):
-    global res
-    subset=[]
-    index=0
-    subsetutil(arr, subset, index)
-    return
-array=[1,2,2]
-subset(array)
+    return ans
+
+ans=[] 
+subset=[]
+index=0
+arr=[1,2,2]
+subsetutil(arr, subset, index,ans)
+print(*ans)
