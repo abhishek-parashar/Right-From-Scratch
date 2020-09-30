3220549caeab068c4e122663cb587495fc2b7277
abhishek-parashar <abhishekparashar@ieee.org>
2020-09-30 16:15:58 +0530

    Backtracking

--- Competitive Coding/combination-1.py
+++ Competitive Coding/combination-1.py
@@ -5,20 +5,20 @@ Created on Tue Sep 29 15:51:01 2020
 @author: Abhishek Parashar
 """
 
-def subsetutil(arr, subset, index,k):
+def subsetutil(arr, subset, index,k,ans):
     if len(subset)==k:
-        print(*subset)
+        ans.append(subset[:])
+        return
     for i in range(index,len(arr)):
         subset.append(arr[i])
-        subsetutil(arr,subset,i+1,k)
+        subsetutil(arr,subset,i+1,k,ans)
         subset.pop(-1)
-    return
-def subset(arr):
-    global res
-    subset=[]
-    index=0
-    k=2
-    subsetutil(arr, subset, index,k)
-    return
-array=[1,2,3]
-subset(array)
+    return ans
+
+subset=[]
+index=0
+k=2
+arr=[1,2,3]
+ans=[]
+subsetutil(arr, subset, index,k,ans)
+print(*ans)
