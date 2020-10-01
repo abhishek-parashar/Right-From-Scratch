a8fbf77a74d903d8d4205a3d8970d9255d7ebc1a
abhishek-parashar <abhishekparashar@ieee.org>
2020-10-01 13:07:40 +0530

    stack

---
+++ Competitive Coding/next-greater-left.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct  1 11:49:27 2020
+
+@author: Abhishek Parashar
+"""
+
+
+stack=[]
+vector=[]
+arr=[1,3,2,4]
+for i in range(len(arr)-1):
+    if (len(stack)==0):
+        vector.append(-1)
+    elif (len(stack)>0 and stack[-1]>arr[i]):
+        vector.append(stack[-1])
+    elif (len(stack)>0 and stack[-1]<=arr[i]):
+        while(len(stack)>0 and stack[-1]<=arr[i]):
+            stack.pop(-1)
+        if(len(stack)==0):
+            vector.append(-1)
+        else:
+            vector.append(stack[-1])
+    stack.append(arr[i])
+print(vector)
+            
