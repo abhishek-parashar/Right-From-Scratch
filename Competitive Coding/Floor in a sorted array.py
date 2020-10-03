cdb9f561f6fbe21da6adcc51f7db403124981290
abhishek-parashar <abhishekparashar@ieee.org>
2020-10-03 16:54:32 +0530

    binary search

--- Competitive Coding/Floor in a sorted array.py
+++ Competitive Coding/Floor in a sorted array.py
@@ -1,22 +1,34 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Sep 14 19:32:44 2020
+Created on Sat Oct  3 16:27:47 2020
 
 @author: Abhishek Parashar
 """
+
 t=int(input())
 while(t>0):
     inpt=list(map(int,input().split()))
-    a=list(map(int,input().split()))
+    arr=list(map(int,input().split()))
     n=inpt[0]
     x=inpt[1]
-    s=[]
-    for i in range(n+1):
-        if(a[0]>x):
-            print(-1)
-            break
-        elif(a[i]<x):
-            s.append(a[i])
-        print(s[-1])
+    end=len(arr)-1
+    start=0
+   
+    def search(arr,start,end,x):
+        mid=(start+end)//2
+        if(start>end):
+            return -1
+        if(arr[mid-1]<x and arr[mid]>x):
+            return arr[mid-1]
+        if (arr[end]<x):
+            return arr[end]
+        if(arr[mid]==x):
+            return arr[mid]
+        elif(arr[mid]<x):
+            return search(arr,mid+1,end,x)
+        else:
+            return search(arr,start,mid-1,x)
+        
+    s=search(arr,start,end,x)
+    print(s)
     t=t-1
-    
