e07c4cf6337ce3655e2c49bed3d97d9561820f43
abhishek-parashar <abhishekparashar@ieee.org>
2020-11-07 17:06:45 +0530

    array

---
+++ Competitive Coding/pow(x,n).py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov  7 10:52:41 2020
+
+@author: Abhishek Parashar
+"""
+
+def power(x, y): 
+  
+    if(y == 0): return 1
+    temp = power(x, int(y / 2))  
+      
+    if (y % 2 == 0): 
+        return temp * temp 
+    else: 
+        if(y > 0): return x * temp * temp 
+        else: return (temp * temp) / x 
+      
+# Driver Code 
+x, y = 2, -3
+print('%.6f' %(power(x, y))) 
+    
+
