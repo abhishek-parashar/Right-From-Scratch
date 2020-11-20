887abe04593855bd42ccd3e57efe185e488a920b
abhishek-parashar <abhishekparashar@ieee.org>
2020-11-20 13:16:26 +0530

    hashing

---
+++ Competitive Coding/2 sum.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 20 13:14:16 2020
+
+@author: Abhishek Parashar
+"""
+
+class Solution(object):
+   def twoSum(self, nums, target):
+      """
+      :type nums: List[int]
+      :type target: int
+      :rtype: List[int]
+      """
+      required = {}
+      for i in range(len(nums)):
+         if target - nums[i] in required:
+            return [required[target - nums[i]],i]
+         else:
+            required[nums[i]]=i
+input_list = [2,8,12,15]
+ob1 = Solution()
+print(ob1.twoSum(input_list, 20))
